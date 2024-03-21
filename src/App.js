538a641c09be4d8c547d5941e5b8002a5bc33d8a
nathan-burd <nathanburd.dev@gmail.com>
2024-03-20 23:42:34 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+import Clubhouse from './screens/Clubhouse';
+import LandingPage from './screens/LandingPage';
+import StatScreen from './screens/StatScreen';
 
 function App() {
+
+  const location = useLocation();
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    
+      <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<LandingPage/>}></Route>
+            <Route path="/Clubhouse" element={<Clubhouse/>}></Route>
+            <Route path="/Dashboard" element={<AboutScreen/>}></Route>
+          </Routes>
+        
+      </AnimatePresence>
+    
   );
 }
 
