cee1f8c003492935c2f9b9549fc1346e8e61a8de
nathan-burd <nathanburd.dev@gmail.com>
2024-03-20 23:35:51 -0400

    Added basic clubhouse components

---
+++ src/components/GolfBag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GolfBag() {
+  return (
+    <div>GolfBag</div>
+  )
+}
+
+export default GolfBag
---
+++ src/components/RoundList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RoundList() {
+  return (
+    <div>RoundList</div>
+  )
+}
+
+export default RoundList
