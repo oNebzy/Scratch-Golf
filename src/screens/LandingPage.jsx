d2a5ecbe6daaa25e53a76cb4fcf616942fa829f9
nathan-burd <nathanburd.dev@gmail.com>
2024-03-25 22:09:10 -0400

    roadmapped landing page layout

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div>
+      <div>need login components on the left in a floating box</div>
+      <div>maybe gradient 'glass covered' animated background on the left behind the login box</div>
+      <div>golf gif on the right?</div>
+    </div>
   )
 }
 
